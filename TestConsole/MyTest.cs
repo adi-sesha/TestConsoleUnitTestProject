c07ef89108b14df3e5cb78794a2f6ca4b02c48ec
Adi <Adi>
2014-07-02 13:55:30 +0530

    Adding project

---
+++ TestConsole/MyTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class MyTest
+    {
+        public static void Main(string[] args)
+        {
+            MyTest myTest = new MyTest();
+            Console.WriteLine("Welcome" + "\n");
+            Console.WriteLine(myTest.getHello());
+            Console.ReadLine();
+        }
+
+        public string getHello()
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ TestConsoleUnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestConsole;
+
+namespace TestConsoleUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {            
+            TestConsole.MyTest tt = new TestConsole.MyTest();
+            Assert.AreEqual("Hello", tt.getHello());
+            
+        }
+    }
+}
